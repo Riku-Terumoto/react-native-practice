da693700135873eab8d8ecf44e1269e4ac1b339d
terumotoriku <081kng@gmail.com>
2023-02-10 08:32:52 +0900

    Replace KeyboardAvoidingView with KeyboardSafeView

--- App.jsx
+++ App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';s
-// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     <>
       {/* <MemoListScreen /> */}
       {/* <MemoDetailScreen /> */}
-      {/* <MemoEditScreen /> */}
-      <MemoCreateScreen />
+      <MemoEditScreen />
+      {/* <MemoCreateScreen /> */}
     </>
   );
 }
