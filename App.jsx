6c0a70866d251531a5590678cebddec1cf480d2a
terumotoriku <081kng@gmail.com>
2023-02-10 08:18:38 +0900

    Create MemoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import MemoListScreen from './src/screens/MemoListScreen';
+import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';s
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     <>
-      <MemoListScreen />
+      {/* <MemoListScreen /> */}
       {/* <MemoDetailScreen /> */}
+      {/* <MemoEditScreen /> */}
+      <MemoCreateScreen />
     </>
   );
 }
