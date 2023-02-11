77e5b397e06401a5bce04a678fdeb9db3b54070a
terumotoriku <081kng@gmail.com>
2023-02-11 18:46:16 +0900

    Create LogInScreen and SignUpScreen

--- App.jsx
+++ App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import SignUpScreen from './src/screens/SignUpScreen';
+// import LogInScreen from './src/screens/LogInScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';s
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 
 export default function App() {
   return (
     <>
       {/* <MemoListScreen /> */}
       {/* <MemoDetailScreen /> */}
-      <MemoEditScreen />
+      {/* <MemoEditScreen /> */}
       {/* <MemoCreateScreen /> */}
+      {/* <LogInScreen /> */}
+      <SignUpScreen />
     </>
   );
 }
